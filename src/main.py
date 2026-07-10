a8bfed22677f756fc21546f49930d21f8102b301
ShreyaTiwari190105 <shreyat190105@gmail.com>
2026-07-10 19:59:41 +0530

    Read and display authentication logs

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-print("Welcome to LogSentry!")
+from pathlib import Path
+
+print("="* 40)
+print("          LogSentry")
+print("="* 40)
+print()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+log_file = BASE_DIR / "sample_logs" / "auth.log"
+
+print("Reading authentication log...")
+
+with open(log_file, "r") as file:
+    logs = file.readlines()
+
+print()
+print(f"Total Log Entries: {len(logs)}")
+
+print()
+
+for index,log in enumerate(logs,start=1):
+    print("-"* 40)
+    print(f"Entry {index}")
+    print(log.strip())
